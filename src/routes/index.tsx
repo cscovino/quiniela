9a7effbf5b632cc6fe909b81308dbad525c06f6b
Carlos Scovino <kikescovino@gmail.com>
2022-11-08 13:27:47 -0300

    feat: add not found view

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,21 +4,25 @@ import Home from '@/views/Home';
 import GroupStage from '@/views/GroupStage';
 import Preview from '@/views/Preview';
 import Playoffs from '@/views/Playoffs';
+import NotFound from '@/views/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: 'group-stage',
     element: <GroupStage />,
+    errorElement: <NotFound />,
   },
   {
     path: 'group-stage/preview',
     element: <Preview />,
+    errorElement: <NotFound />,
   },
-  { path: 'playoffs', element: <Playoffs /> },
+  { path: 'playoffs', element: <Playoffs />, errorElement: <NotFound /> },
 ]);
 
 export default router;
---
+++ src/views/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import { Flex, Image } from '@chakra-ui/react';
+
+function NotFound() {
+  return (
+    <Flex direction="column" alignItems="center" justify="center" width="100%" height="100%">
+      <Image src="/qatar-2022.svg" alt="FIFA-World-Cup-Qatar-2022" height="50%" />
+    </Flex>
+  );
+}
+
+export default NotFound;
