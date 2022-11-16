d5c97520f07d96626b25c5b3ce5795f31568499d
Carlos Scovino <kikescovino@gmail.com>
2022-11-16 18:37:46 -0300

    feat: add participants table

---
+++ src/views/ParticipantsTable/i18n.ts
@@ -0,0 +1,18 @@
+import i18next from '@/configs/i18n';
+import { LOCALES } from '@/helpers/locales';
+
+i18next.addResources(LOCALES.ES, 'TABLE', {
+  TITLE: 'Tabla de posiciones',
+  HOME: 'Inicio',
+  PARTICIPANT: 'Participante',
+  POINTS: 'Puntos',
+  SCORER: 'Goleador',
+  GROUP: 'Grupo',
+  GROUP_FIRST: '1°',
+  GROUP_SECOND: '2°',
+  FINAL_POSITION: 'Final',
+  FINAL_FIRST: '1°',
+  FINAL_SECOND: '2°',
+  FINAL_THIRD: '3°',
+  FINAL_FOURTH: '4°',
+});
