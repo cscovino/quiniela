39a1d1d586e27a052bf7ea96cf02e2dc8085471d
Carlos Scovino <kikescovino@gmail.com>
2022-11-20 13:04:43 -0300

    feat: close form

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Image, Link, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+import { Button, Flex, Image, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import i18next from 'i18next';
 
@@ -8,9 +8,9 @@ const rules = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function Home() {
   const navigate = useNavigate();
-  const onClick = () => {
-    navigate('/group-stage');
-  };
+  // const onClick = () => {
+  //   navigate('/group-stage');
+  // };
   const onClickParticipants = () => {
     navigate('/participants');
   };
@@ -29,34 +29,40 @@ function Home() {
           <ListItem key={`RULE-${item}`}>{i18next.t<string>(`HOME:RULES.${item}`)}</ListItem>
         ))}
       </UnorderedList>
-      <Text textAlign="center" fontSize="lg" fontFamily="qatar" color="white">
-        {i18next.t<string>('HOME:BYE')}
-      </Text>
-      <Text textAlign="center" fontSize="md" fontFamily="qatar" color="white" marginTop="20px">
-        <Link
-          href="https://firebasestorage.googleapis.com/v0/b/quiniela-mundial-d4a88.appspot.com/o/Quiniela%20Qatar%202022.xlsx?alt=media&token=361676b8-c508-427a-95c3-6ef6aa644911"
-          target="_blank"
-          color="#fa5d84"
-        >
-          {i18next.t<string>('HOME:LINK')}
-        </Link>
-        {i18next.t<string>('HOME:HELP')}
-      </Text>
+      {/* <Text textAlign="center" fontSize="lg" fontFamily="qatar" color="white"> */}
+      {/*   {i18next.t<string>('HOME:BYE')} */}
+      {/* </Text> */}
+      {/* <Text
+            textAlign="center"
+            fontSize="md"
+            fontFamily="qatar"
+            color="white"
+            marginTop="20px"
+          > */}
+      {/*   <Link */}
+      {/*     href="" */}
+      {/*     target="_blank" */}
+      {/*     color="#fa5d84" */}
+      {/*   > */}
+      {/*     {i18next.t<string>('HOME:LINK')} */}
+      {/*   </Link> */}
+      {/*   {i18next.t<string>('HOME:HELP')} */}
+      {/* </Text> */}
       <Flex direction="row" justify="center" gap={0}>
-        <Button
-          alignSelf="center"
-          marginY="20px"
-          marginX="10px"
-          height="50px"
-          fontSize="25px"
-          color="#6f0625"
-          bg="#fee1d2"
-          _hover={{ bg: '#fa5d84' }}
-          fontFamily="qatar"
-          onClick={onClick}
-        >
-          {i18next.t<string>('HOME:NEXT')}
-        </Button>
+        {/* <Button */}
+        {/*   alignSelf="center" */}
+        {/*   marginY="20px" */}
+        {/*   marginX="10px" */}
+        {/*   height="50px" */}
+        {/*   fontSize="25px" */}
+        {/*   color="#6f0625" */}
+        {/*   bg="#fee1d2" */}
+        {/*   _hover={{ bg: '#fa5d84' }} */}
+        {/*   fontFamily="qatar" */}
+        {/*   onClick={onClick} */}
+        {/* > */}
+        {/*   {i18next.t<string>('HOME:NEXT')} */}
+        {/* </Button> */}
         <Button
           alignSelf="center"
           marginY="20px"
